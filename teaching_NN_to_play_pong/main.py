3505844a47364e21e923baab5974f73f80cb4144
kotsipiter <kostaspappas2000@hotmail.com>
2023-11-06 18:13:31 +0200

    seperated classes and made it into package

--- teaching_NN_to_play_pong/main.py
+++ teaching_NN_to_play_pong/main.py
@@ -1,5 +1,21 @@
-from pong import *
+import pygame
+from pong import Game
 
-pong = pong(7,7,0,4)
+width, height = 700,500
+window = pygame.display.set_mode((width,height))
 
-pong.play_pong()
+game = Game(window, width, height)
+
+run = True
+
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            break
+
+    game.loop()
+    game.draw()
+    pygame.display.update()
+
+pygame.quit()    
